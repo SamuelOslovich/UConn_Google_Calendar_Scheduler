75b98061150d0f72ca09183dde472c4149e42d61
Samuel Oslovich <59121754+SamuelOslovich@users.noreply.github.com>
2020-01-17 19:43:24 -0500

    Added GUI

--- UConn_Google_Calendar_Inputter.py
+++ UConn_Google_Calendar_Inputter.py
@@ -4,13 +4,37 @@ import pickle
 import datefinder
 import datetime
 from datetime import timedelta, timezone
-import Txt_To_Calendar_Input
+import PySimpleGUI as sg
+
+#Gets the user input using a GUI
+#A dark blue theme similar to the UConn color scheme
+sg.theme('DarkBlue13')
+
+#The text and buttons inside the window
+layout = [  [sg.Text('Paste your class schedule here'), sg.InputText()],
+            [sg.Button('Enter'), sg.Button('Cancel')] ]
+
+# Create the Window
+window = sg.Window('UConn Class Schedule Inputter', layout)
+
+event, values = window.read()
+
+#Opens a file to store user input
+inputFile = open("InputFile.txt", "w")
+
+#Writes the user input to the calendar
+inputFile.write(values[0])
+inputFile.close()
+
+#Close the window
+window.close()
+
 
 scopes = ['https://www.googleapis.com/auth/calendar']
 
 #Prompts the user to authorize the application
 flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", scopes=scopes)
-credentials = flow.run_console()
+credentials = flow.run_local_server(port=0)
 
 #Stores the credentials
 pickle.dump(credentials, open("token.pkl", "wb")) 
@@ -61,8 +85,131 @@ def create_event(summary, description, start_time, end_time, location=None, repe
 
     event = service.events().insert(calendarId='primary', body=event).execute()
 
+def run():
+    #Opens the user input file
+    fileInput = open("InputFile.txt", "r")
+
+    #Creates a list to store information about each class
+    classInfo = []
+
+    #A variable that keeps track of the first time through the loop
+    first = True
+
+    #Operates on every line of the file and if the line contains class info appends it to the list
+    for line in fileInput.readlines():
+        
+        if "Schedule" not in line:
+            
+            if "Academic Calendar Deadlines" in line:
+                #The first time through no class info has been read yet
+                if first:
+                    first = False
+                else:
+                    #Shows an end to information for a class
+                    classInfo.append("|")
+
+            else:
+                #Adds the class info to the list
+                classInfo.append(line)
+
+    fileInput.close()
+
+    #Shows an end to information for the last class
+    classInfo.append("|")
+
+    #Converts the class info into a list of lists where each sublist contains the information for a class
+    def classInfoList(classInfo):
+        listOfClassInfo = []
+        
+        listAcc = []
+
+        for info in classInfo:
+            if "|" in info:
+                listOfClassInfo.append(listAcc)
+                listAcc = []
+            else:
+                listAcc.append(info)
+            
+        return listOfClassInfo
+
+
+    #Returns a list of dates with start time and end time
+    def dayTimeConverter(dayTime):
+
+        days = []
+        split = dayTime.split()
+        times = [split[1], split[3]]
+
+        if "Mo" in dayTime:
+            days.append(["1/27/2020", times]) #1/27 is the first monday of the semester
+        if "Tu" in dayTime:
+            days.append(["1/21/2020", times]) #1/21 is the first tuesday of the semester
+        if "We" in dayTime:
+            days.append(["1/22/2020", times]) #etc
+        if "Th" in dayTime:
+            days.append(["1/23/2020", times])
+        if "Fr" in dayTime:
+            days.append(["1/24/2020", times])
+        if "Sa" in dayTime:
+            days.append(["1/25/2020", times])
+        if "Su" in dayTime:
+            days.append(["1/26/2020", times])
+
+        return days
+
+
+
+    #Opens the output file
+    output = open("OutputFile.txt", "w")
+
+    #Writes the class to the file
+    def addClass(information):
+        lastDayOfSemester = "5/1/2020"
+
+        #More than 1 time/location
+        if len(information) > 4:
+            information[2] = dayTimeConverter(information[2])
+            for days in information[2]:
+                output.write("%s, %s, %s %s, %s %s, %s, %s" % (information[0].rstrip("\n"), #Class name/number
+                                                                information[1].rstrip("\n"), #Description ie: lec, lab
+                                                                days[0], days[1][0], #Date and start time
+                                                                days[0], days[1][1], #Date and end time
+                                                                information[3].rstrip("\n"), #Location
+                                                                lastDayOfSemester)) #Last day of the semester
+                output.write("\n")
+
+            information[4] = dayTimeConverter(information[4])
+            for days in information[4]:
+                output.write("%s, %s, %s %s, %s %s, %s, %s" % (information[0].rstrip("\n"), #Class name/number
+                                                                information[1].rstrip("\n"), #Description ie: lec, lab
+                                                                days[0], days[1][0], #Date and start time
+                                                                days[0], days[1][1], #Date and end time
+                                                                information[5].rstrip("\n"), #Location
+                                                                lastDayOfSemester)) #Last day of the semester
+                output.write("\n")
+
+        #One time/location
+        elif len(information) > 3:
+            information[2] = dayTimeConverter(information[2])
+            for days in information[2]:
+                output.write("%s, %s, %s %s, %s %s, %s, %s" % (information[0].rstrip("\n"), #Class name/number
+                                                                information[1].rstrip("\n"), #Description ie: lec, lab
+                                                                days[0], days[1][0], #Date and start time
+                                                                days[0], days[1][1], #Date and end time
+                                                                information[3].rstrip("\n"), #Location
+                                                                lastDayOfSemester)) #Last day of the semester
+                output.write("\n")
+            
+            
+
+    classInfo = classInfoList(classInfo)
+    for info in classInfo:
+        addClass(info)
+
+    output.close()
+
 #Converts user input to output that can be read an turned into events in the calendar
-Txt_To_Calendar_Input.run()
+run()
 
 #Creates an event in the users google calendar for each class in the output file
 classes = open("OutputFile.txt", "r")
@@ -74,3 +221,5 @@ for line in classes.readlines():
               classList[3],   #End time
               classList[4],   #Location
               classList[5])   #Repeat weekly until
+
+classes.close()
